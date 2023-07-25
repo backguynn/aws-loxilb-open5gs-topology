7e897943d3663f78c2c486d2a8f2a0f066e1e033
backguynn <backguyn@netlox.io>
2023-07-25 17:10:44 +0900

    add terraform script using AMI version

---
+++ use-ami/outputs.tf
@@ -0,0 +1,48 @@
+# Copyright (c) HashiCorp, Inc.
+# SPDX-License-Identifier: MPL-2.0
+
+output "cluster_endpoint" {
+  description = "Endpoint for EKS control plane"
+  value       = module.eks.cluster_endpoint
+}
+
+output "cluster_security_group_id" {
+  description = "Security group ids attached to the cluster control plane"
+  value       = module.eks.cluster_security_group_id
+}
+
+output "region" {
+  description = "AWS region"
+  value       = var.region
+}
+
+output "cluster_name" {
+  description = "Kubernetes Cluster Name"
+  value       = module.eks.cluster_name
+}
+
+output "node_security_group_id" {
+  description = "Security group ids attached to the node control plane"
+  value       = module.eks.node_security_group_id
+}
+
+
+output "loxilb_private_ip" {
+  description = "loxilb private ip address"
+  value       = module.ec2_instance.private_ip
+}
+
+output "loxilb_public_ip" {
+  description = "loxilb public ip address"
+  value       = module.ec2_instance.public_ip
+}
+
+output "upf_public_ip" {
+  description = "UPF public ip address"
+  value       = module.upf_ec2_instance.public_ip
+}
+
+output "ue_public_ip" {
+  description = "UE public ip address"
+  value       = module.ue_ec2_instance.public_ip
+}
